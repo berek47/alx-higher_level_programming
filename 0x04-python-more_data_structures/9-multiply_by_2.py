7cca10bf4b4dbcf08d228d1fd312e395401814ab
root <berekettadessekuche75@gmail.com>
2022-09-28 15:31:50 -0700

    python-more_data_structures

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    keys = a_dictionary.keys()
+    for i in keys:
+        new_dict[i] = new_dict[i] * 2
+    return new_dict
